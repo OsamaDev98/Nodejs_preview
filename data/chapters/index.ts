f91cb8312a0f24aad1cb8b5dd93cf259a5f7dcdf
Osama Gad <116967770+OsamaDev98@users.noreply.github.com>
2026-09-12 21:23:55 +0300

    refactor: compose study chapters from modular content files

---
+++ data/chapters/index.ts
@@ -0,0 +1,21 @@
+import { eventLoopChapter } from "./event-loop";
+import { filesBuffersChapter } from "./files-buffers";
+import { foundationsChapter } from "./foundations";
+import { libuvThreadpoolChapter } from "./libuv-threadpool";
+import { microtasksChapter } from "./microtasks";
+import { modulesChapter } from "./modules";
+import { streamsNpmChapter } from "./streams-npm";
+import { workersPerformanceChapter } from "./workers-performance";
+
+export const chapters = [
+  foundationsChapter,
+  modulesChapter,
+  filesBuffersChapter,
+  streamsNpmChapter,
+  libuvThreadpoolChapter,
+  eventLoopChapter,
+  microtasksChapter,
+  workersPerformanceChapter,
+];
+
+export const totalReadingMinutes = 385;
